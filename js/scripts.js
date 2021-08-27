7ca51dc9dd256c415eccd79bebbd8622c0c21434
Sunshinepunch <78851729+Sunshinepunch@users.noreply.github.com>
2021-08-27 14:22:45 -0700

    toppings are added to pizza property when selected

--- js/scripts.js
+++ js/scripts.js
@@ -46,22 +46,35 @@ Pizza.prototype.findCost = function () {
 
 
 //UI Logic
+
+
+function myFunction() {
+  let toppingOne = document.getElementById("first").value;
+  let toppingTwo = document.getElementById("second").value;
+  let toppingThree = document.getElementById("third").value;
+  newPizza.toppings.push(toppingOne);
+  newPizza.toppings.push(toppingTwo);
+  newPizza.toppings.push(toppingThree);
+  
+  console.log(newPizza.toppings);
+}
+
+
+
 let newPizza = new Pizza ([], "");
 $(document).ready(function() {
   $("form#pizza-order").submit(function (event) {
     event.preventDefault();
-
-  });
-  $('#pizza-size').submit(function() {
     let size = parseInt($("input:radio[name=size]").val());
-    if (size == 1){
+    if (size === 1){
       newPizza.size = "Candle";
-    } else if (size == 2) {
+    } else if (size === 2) {
       newPizza.size = "Streetlight";
-    } else if (size == 3) {
+    } else if (size === 3) {
       newPizza.size = "Sunny";
     } else {
       newPizza.size = "Supernova";
     }
-  })
+    console.log(newPizza)
+  });
 });
